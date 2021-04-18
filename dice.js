f6e72eea38f21dbc24710a8240d82c97bf042113
Puneet Bisht <77060581+appuneet@users.noreply.github.com>
2021-04-18 12:32:04 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,27 +1,45 @@
-document.getElementById("btn").onclick = function() {myFunc()}
+// taking names of players.
+var n1 = prompt("Enter player 1's name:");
+var n2 = prompt("Enter player 2's name:");
 
-function myFunc(){
-// this is random dice number for player 1.
-var p1 = Math.round(Math.random()*5);
+// capitalise word.
+var name1 = n1.toUpperCase();
+var name2 = n2.toUpperCase();
 
-// this is random dice number for player 2.
-var p2 = Math.round(Math.random()*5);
 
-// array of all images.
-var myPix = ["images/dice1.png","images/dice2.png","images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+// changing name.
+document.querySelector(".dice #plr1").textContent = name1;
+document.querySelector(".dice #plr2").textContent = name2;
 
-// commands to change the winning line.
-if (p1>p2){
-    document.querySelector(".container h1").textContent="Player 1 Wins."
-}
-else if (p1===p2){
-    document.querySelector(".container h1").textContent="It's a tie."
-}
-else {
-    document.querySelector(".container h1").textContent="Player 2 Wins."
-}
+// changing heading as required.
+document.querySelector("#head").textContent = name1 + " V/S " + name2;
 
-// changing image by accessing array myPix.
-document.querySelector(".img1").src=myPix[p1]
-document.querySelector(".img2").src=myPix[p2]
+// onclick function. (you can also use other method.)
+document.querySelector("#btn").onclick = function () { myFunc() }
+
+// function that changes the content.
+function myFunc() {
+    // this is random dice number for player 1.
+    var p1 = Math.round(Math.random() * 5);
+
+    // this is random dice number for player 2.
+    var p2 = Math.round(Math.random() * 5);
+
+    // array of all images.
+    var myPix = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+
+    // commands to change the winning line.
+    if (p1 > p2) {
+        document.querySelector("header h1").textContent = "🚩" + name1 + " WINS."
+    }
+    else if (p1 === p2) {
+        document.querySelector("header h1").textContent = "DRAW!"
+    }
+    else {
+        document.querySelector("header h1").textContent = name2 + " WINS.🚩"
+    }
+
+    // changing image by accessing array myPix.
+    document.querySelector(".img1").src = myPix[p1]
+    document.querySelector(".img2").src = myPix[p2]
 }
